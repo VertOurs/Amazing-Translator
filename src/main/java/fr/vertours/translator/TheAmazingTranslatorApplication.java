19f6658a459c75680e8a63ab97133638384329e5
VertOurs <vertours@gmail.com>
2022-03-11 15:28:44 +0100

    First commit

---
+++ src/main/java/fr/vertours/translator/TheAmazingTranslatorApplication.java
@@ -0,0 +1,13 @@
+package fr.vertours.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAmazingTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAmazingTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/vertours/translator/TheAmazingTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vertours.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheAmazingTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
